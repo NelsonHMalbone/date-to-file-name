046e529cff36eb783bea0ac275528dd3ff6126b6
Nelson Malbone <nhmalbone@icloud.com>
2024-10-15 17:42:58 -0400

    10-15-2024
    made a dir to gather content from each file in now able to count and found out how many words is in each file from the files folder

--- tutorial.py
+++ tutorial.py
@@ -1,2 +1,22 @@
 # Udemy 10/15/2024
 # tutorial Add Date to Filenames
+import os
+from datetime import datetime
+
+# starting with the import to the output
+# path should be in the same spot as file so no need for a "/"
+
+directory = 'files'
+
+# get the list of all files in directory
+filenames = os.listdir(directory)
+
+# processing the listdir with a for loop to count words in each file
+for filename in filenames:
+    filepath = os.path.join(directory, filename)
+    # adding word count from each file
+    with open(filepath, 'r') as file: # read thur all the files
+        content = file.read()
+        words = content.split() # spliting each word from content in file
+        word_count = len(words) # getting total amount of words
+        print(word_count)
