4272f72152f42a0b665bf674833342aaccc37773
Nelson Malbone <nhmalbone@icloud.com>
2024-10-16 17:51:07 -0400

    10-16-2024
    Start of the course project for The udemy course
    
    basically making the same project as the day before just with the yyyy-mm-dd
    
    set up line of code to read the files and to have the os gather the date

---
+++ project_description.py
@@ -0,0 +1,22 @@
+import os
+from datetime import datetime
+
+# the directory aka the folder to get files from
+directory = 'files_project'
+
+# getting a list of the dir from the directory variable
+filepath = os.listdir(directory)
+# ran a test to see if sys can see the files inside dic
+print(filepath)
+
+
+for filename in filepath:
+    filepath = os.path.join(directory, filename)
+    with open(filepath, 'r') as file:
+        # read content inside each file
+        content = file.read()
+        # print(content)
+
+    day = datetime.now().strftime('%Y-%m-%d')
+    # test to see if it prints out the yyyy-mm-dd
+    print(day)
